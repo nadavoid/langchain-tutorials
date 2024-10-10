4d208a33775472867b4f5396cd13464ed436b54c
David Lanier <dlanier@phase2technology.com>
2024-10-10 15:38:27 -0700

    Add example code and npm packages the code depends on

--- langchain-js-tutorial-rag/rag.ts
+++ langchain-js-tutorial-rag/rag.ts
@@ -1,4 +1,41 @@
 import "cheerio";
 import dotenv from "dotenv";
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { OpenAIEmbeddings, ChatOpenAI } from "@langchain/openai";
+import { pull } from "langchain/hub";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 
 dotenv.config();
+
+const loader = new CheerioWebBaseLoader(
+  "https://lilianweng.github.io/posts/2023-06-23-agent/"
+);
+
+const docs = await loader.load();
+
+const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+  chunkOverlap: 200,
+});
+const splits = await textSplitter.splitDocuments(docs);
+const vectorStore = await MemoryVectorStore.fromDocuments(
+  splits,
+  new OpenAIEmbeddings()
+);
+
+// Retrieve and generate using the relevant snippets of the blog.
+const retriever = vectorStore.asRetriever();
+const prompt = await pull<ChatPromptTemplate>("rlm/rag-prompt");
+const llm = new ChatOpenAI({ model: "gpt-3.5-turbo", temperature: 0 });
+
+const ragChain = await createStuffDocumentsChain({
+  llm,
+  prompt,
+  outputParser: new StringOutputParser(),
+});
+
+const retrievedDocs = await retriever.invoke("what is task decomposition");
