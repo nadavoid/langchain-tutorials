bd6c90d037b2616bb062d4ee5e5ae6a4e7a8e108
David Lanier <dlanier@phase2technology.com>
2024-11-08 14:40:10 -0800

    Add query that does not use chat history

--- rag-conversational/index.ts
+++ rag-conversational/index.ts
@@ -9,6 +9,67 @@ dotenv.config({
 });
 
 const llm = new ChatOpenAI({
-  model: "gpt-40-mini",
+  model: "gpt-4o-mini",
   temperature: 0,
 });
+
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+
+///////////////////////////////////
+// Version without chat history. //
+///////////////////////////////////
+// 1. Load, chunk and index the contents of the blog to create a retriever.
+const loader = new CheerioWebBaseLoader(
+  "https://lilianweng.github.io/posts/2023-06-23-agent/",
+  {
+    selector: ".post-content, .post-title, .post-header",
+  }
+);
+const docs = await loader.load();
+
+const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+  chunkOverlap: 200,
+});
+const splits = await textSplitter.splitDocuments(docs);
+const vectorstore = await MemoryVectorStore.fromDocuments(
+  splits,
+  new OpenAIEmbeddings()
+);
+const retriever = vectorstore.asRetriever();
+
+// 2. Incorporate the retriever into a question-answering chain.
+const systemPrompt =
+  "You are an assistant for question-answering tasks. " +
+  "Use the following pieces of retrieved context to answer " +
+  "the question. If you don't know the answer, say that you " +
+  "don't know. Use three sentences maximum and keep the " +
+  "answer concise." +
+  "\n\n" +
+  "{context}";
+
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", systemPrompt],
+  ["human", "{input}"],
+]);
+
+const questionAnswerChain = await createStuffDocumentsChain({
+  llm,
+  prompt,
+});
+
+const ragChain = await createRetrievalChain({
+  retriever,
+  combineDocsChain: questionAnswerChain,
+});
+
+const response = await ragChain.invoke({
+  input: "What is the most complex idea in this article?"
+});
+console.log(response.answer);
